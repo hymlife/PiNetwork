c77893d72e2121f8fba6275254a3786856779942
Halil İbrahim Dursun <48247076+hymlife@users.noreply.github.com>
2024-03-18 16:27:00 +0300

    node screen ve pi uygulamlları sayfaları yazıldı

--- src/screens/NodeScreen.js
+++ src/screens/NodeScreen.js
@@ -1,10 +1,99 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import HeaderBack from "../components/HeaderBack";
 
 const NodeScreen = () => {
+  const [node, setNode] = useState("");
+
+  const handleLogin = () => {
+    // Giriş işlemleri burada yapılabilir
+    console.log("Kullanıcı adı:", node);
+  };
+
   return (
-    <View>
-      <Text>NodeScreen</Text>
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <HeaderBack Veri="node" />
+      <View
+        style={{
+          alignItems: "center",
+          justifyContent: "space-between",
+          flexDirection: "column",
+          flex: 1,
+        }}
+      >
+        <View style={{ marginHorizontal: 25 }}>
+          <Text style={{ fontSize: 25 }}>Pİ Node'a girişi onaylayın</Text>
+          <Text style={{ marginTop: 15 }}>
+            Bilgisayarınızda Pi Node uygulamasındaki giriş kodunu yazarak node'a
+            giriş yapın
+          </Text>
+        </View>
+
+        <View style={{ alignItems: "center" }}>
+          <Text style={{ fontSize: 25 }}>Giriş Kodu</Text>
+
+          <TextInput
+            value={node}
+            style={{
+              borderWidth: 1,
+              paddingHorizontal: "25%",
+              paddingVertical: "2%",
+              borderRadius: 15,
+              marginVertical: 5,
+            }}
+            onChangeText={setNode}
+            placeholder="ABCD1234 "
+          />
+
+          <TouchableOpacity
+            onPress={handleLogin}
+            style={{
+              borderWidth: 1,
+              paddingHorizontal: "25%",
+              paddingVertical: "2%",
+              borderRadius: 15,
+            }}
+          >
+            <Text>DEVAM</Text>
+          </TouchableOpacity>
+          <Text style={{ marginTop: 15 }}>
+            pi Node uygulamasını şuradan indirin
+          </Text>
+          <TouchableOpacity>
+            <Text style={{ marginTop: 15 }}>node.minepi.com</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View
+          style={{
+            margin: 20,
+            backgroundColor: "yellow",
+            borderRadius: 5,
+            padding: 10,
+            borderWidth: 2,
+            borderColor: "red",
+          }}
+        >
+          <Text style={{ fontSize: 15 }}>
+            node.inepi.com dışında hiçbir kaynaktanda indirilmiş node yazılımını
+            yüklemeyin
+          </Text>
+          <Text>
+            bu sayfada resmi pi node uygulaması dışında üretilmiş hiçbir kod
+            girmeyin bunu yapmak hesabınızın çalınmasına sebep olabilir
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
--- src/screens/PiUygulamalariScreen.js
+++ src/screens/PiUygulamalariScreen.js
@@ -1,14 +1,67 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
+import HeaderBack from "../components/HeaderBack";
+
+import { Ionicons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const PiUygulamalariScreen = () => {
   return (
     <View>
-      <Text>PiUygulamalariScreen</Text>
+      <HeaderBack Veri="Pi Uygulamaları" />
+
+      <View style={styles.buttonArea}>
+        <TouchableOpacity style={styles.button}>
+          <Ionicons name="chatbox-outline" size={40} color="Pink" />
+          <Text>chat</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <AntDesign name="wallet" size={40} color="black" />
+          <Text>Wallet</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <FontAwesome name="chain-broken" size={40} color="black" />
+          <Text>Blockchain</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.buttonArea2}>
+        <TouchableOpacity style={styles.button}>
+          <MaterialIcons name="developer-mode" size={40} color="black" />
+          <Text>chat</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <AntDesign name="idcard" size={40} color="black" />
+          <Text>Wallet</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 export default PiUygulamalariScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  buttonArea: {
+    flexDirection: "row",
+    margin: "15%",
+    justifyContent: "space-between",
+  },
+  buttonArea2: {
+    flexDirection: "row",
+    marginHorizontal: "20%",
+    justifyContent: "space-between",
+  },
+
+  button: {
+    borderWidth: 1,
+    borderRadius: 15,
+    padding: 10,
+    alignItems: "center",
+  },
+});
